9fd6d62c07f4c71a2d95d9469550d0c4fd8f260a
Zach Toogood <zrtoogood@gmail.com>
2022-01-06 13:07:25 +0200

    [chore] More renaming

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -965,18 +965,18 @@ int32 map_cleanup(time_point tick, CTaskMgr::CTask* PTask)
 
 /************************************************************************
  *                                                                       *
- *  Map-Server Version Screen [venom]                                    *
+ *  zone-server Version Screen [venom]                                    *
  *                                                                       *
  ************************************************************************/
 
 void map_helpscreen(int32 flag)
 {
-    ShowMessage("Usage: map-server [options]");
+    ShowMessage("Usage: zone-server [options]");
     ShowMessage("Options:");
     ShowMessage("  Commands\t\t\tDescription");
     ShowMessage("-----------------------------------------------------------------------------");
     ShowMessage("  --help, --h, --?, /?     Displays this help screen");
-    ShowMessage("  --map-config <file>      Load map-server configuration from <file>");
+    ShowMessage("  --map-config <file>      Load zone-server configuration from <file>");
     ShowMessage("  --version, --v, -v, /v   Displays the server's version");
     ShowMessage("");
     if (flag)
@@ -987,7 +987,7 @@ void map_helpscreen(int32 flag)
 
 /************************************************************************
  *                                                                       *
- *  Map-Server Version Screen [venom]                                    *
+ *  zone-server Version Screen [venom]                                    *
  *                                                                       *
  ************************************************************************/
 
@@ -1114,7 +1114,7 @@ int32 map_config_from_env()
 
 /************************************************************************
  *                                                                       *
- *  Map-Server Config [venom]                                            *
+ *  zone-server Config [venom]                                            *
  *                                                                       *
  ************************************************************************/
 
@@ -1584,9 +1584,9 @@ void log_init(int argc, char** argv)
     std::string logFile;
 #ifdef DEBUGLOGMAP
 #ifdef WIN32
-    logFile = "log\\map-server.log";
+    logFile = "log\\zone-server.log";
 #else
-    logFile = "log/map-server.log";
+    logFile = "log/zone-server.log";
 #endif
 #endif
     bool defaultname = true;
